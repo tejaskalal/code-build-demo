52100c12b747e933cf0ce7e66f974b1e8e285d05
tejaskalal <168655451+tejaskalal@users.noreply.github.com>
2026-04-06 17:36:17 +0530

    Delete simple-node-app/app.js

--- simple-node-app/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end("Hello from CodeBuild ");
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
